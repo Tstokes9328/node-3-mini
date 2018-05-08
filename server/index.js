4e46c5b6dcb122344b591ad00d5f4c335da01b49
Tayte Stokes <taytestokes9328@gmail.com>
2018-05-08 13:41:39 -0600

    Followed along the mini project

--- server/index.js
+++ server/index.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const session = require('express-session');
+const dotenv = require('dotenv');
+
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter');
 
 const app = express();
 
+require('dotenv').config();
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
 
-app.post( "/api/messages", mc.create );
-app.get( "/api/messages", mc.read );
-app.put( "/api/messages", mc.update );
-app.delete( "/api/messages", mc.delete );
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure:false,
+        maxAge: 10000
+    }
 
-const port = process.env.PORT || 3000
+}))
+
+
+app.use((req, res, next) => createInitialSession(req, res, next));
+app.use((req, res, next) => {
+    const {method} = req;
+    if(method === 'POST' || method === 'PUT'){
+        filter(req, res, next)
+    } else {
+        next();
+    }
+})
+
+
+app.post( "/api/messages",  filter, mc.create );
+app.get( "/api/messages", mc.read );
+app.put( "/api/messages", filter, mc.update );
+app.delete( "/api/messages", mc.delete );
+app.get("/api/messages/history", mc.history);
+
+const port = process.env.PORT || 3003
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
